3a9c06a42c5a2ff10a47a06165ffffe79c2af84e
ShaneP2 <21shane.pearson@boiseschools.net>
2020-08-20 15:00:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class main {
+
+
+  public static void main(String[] args) {
+    System.out.println("Run away with me");
+    System.out.print(" ");
+    System.out.print("Lost souls and reverie");
+    System.out.println("");
+    System.out.println("Running wild and running free");
+    System.out.println("Two kids, you and me");
+    System.out.println("");
+    System.out.println("And I say");
+    System.out.println("Hey, hey hey hey");
+    System.out.println("Living like we're renegades");
+  }
+}
+/*1: Run away with me
+2: Lost souls and reverie
+3: Running wild and running free
+4: Two kids, you and me
+
+5: And I say
+6: Hey, hey hey hey
+7: Living like we're renegades
+*/
